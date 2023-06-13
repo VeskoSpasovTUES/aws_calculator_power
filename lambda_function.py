9d7f66a22a12b76328d65b387a40da7f668d424c
VeskoSpasovTUES <127239824+VeskoSpasovTUES@users.noreply.github.com>
2023-06-13 12:12:24 +0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,16 +3,13 @@ import json
 # import the Python math library
 import math
 
-# import the AWS SDK (for Python the package name is boto3)
-import boto3
-
 # define the handler function that the Lambda service will use an entry point
 def lambda_handler(event, context):
 
 # extract the two numbers from the Lambda service's event object
-    mathResult = math.sqrt(int(event['base']))
+    mathResult = math.pow(int(event['base']), int(event['exponent']))
 
-# return a properly formatted JSON object
+    # return a properly formatted JSON object
     return {
     'statusCode': 200,
     'body': json.dumps('Your result is ' + str(mathResult))
